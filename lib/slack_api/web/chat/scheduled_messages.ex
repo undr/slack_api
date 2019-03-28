eb13d1597f4cce8b1d62032c77ff1f01a98237a6
undr <undr@yandex.ru>
2019-03-28 10:40:12 +0300

    Fix chat.scheduledMessages.all request

--- lib/slack_api/web/chat/scheduled_messages.ex
+++ lib/slack_api/web/chat/scheduled_messages.ex
@@ -2,5 +2,5 @@ defmodule SlackAPI.Web.Chat.ScheduledMessages do
   alias SlackAPI.Web
 
   def all(client, params \\ %{}),
-    do: Web.post(client, "chat.scheduledMessages.list", params)
+    do: Web.post(client, "chat.scheduledMessages.list", {:json, params})
 end
